d4be5bd27eeb936fe273c3fb3917064909ced735
Willkommen <turash@gmx.de>
2019-01-13 21:21:41 +0600

    finished styling auth views

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,5 +5,10 @@ import Buefy from 'buefy';
 
 Vue.use(Buefy);
 
+var app = new Vue({
+    el: '#app',
+    data: {}
+});
+
 
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
